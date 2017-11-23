f568a5ee82af7a4f1198f85785fd8d26356f6cac
Marcello Alvarado <marsh@Marcellos-iMac.local>
2017-11-22 19:38:23 -0800

    shop by lifestyle page first section complete

--- assets/theme.js
+++ assets/theme.js
@@ -3037,7 +3037,37 @@ $('.slider-nav').slick({
    asNavFor: '.slider-for',
    focusOnSelect: true,
    vertical: true,
-   verticalSwiping: true
+   verticalSwiping: true,
+   responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        vertical: false,
+        verticalSwiping: false
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
 
 // $('#get-slider-thumbnails-container').css({'height': $('#get-slider-container').height(),'overflow':'hidden'});
